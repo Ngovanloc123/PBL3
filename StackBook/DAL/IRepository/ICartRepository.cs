348a7368e333b3b24954e2a8c0a8e363d7166578
haduckien <haduckien1709@gmail.com>
2025-04-22 17:41:39 +0700

    add CartRepository

--- StackBook/DAL/IRepository/ICartRepository.cs
+++ StackBook/DAL/IRepository/ICartRepository.cs
@@ -1,40 +1,32 @@
-// using StackBook.Models;
-// using System;
-// using System.Collections.Generic;
-// using System.Threading.Tasks;
+using StackBook.DTOs;
+using StackBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-// namespace StackBook.DAL.IRepository
-// {
-//     public interface ICartRepository
-//     {
-//         // Lấy cart theo user, nếu chưa có thì null
-//         Task<Cart?> GetByUserIdAsync(Guid userId);
+namespace StackBook.DAL.IRepository
+{
+    public interface ICartRepository
+    {
+        // Lấy cart theo user, nếu chưa có thì null
+        Task<Cart?> GetByUserIdAsync(Guid userId);
 
-//         // Lấy cart, nếu chưa có thì tạo mới
-//         Task<Cart> GetOrCreateByUserIdAsync(Guid userId);
+        // Lấy cart, nếu chưa có thì tạo mới
+        Task<Cart> GetOrCreateByUserIdAsync(Guid userId);
 
-//         // Lấy chi tiết giỏ hàng
-//         Task<List<CartDetail>> GetCartDetailsAsync(Guid userId);
+        // Lấy chi tiết giỏ hàng
+        Task<List<BookInCartDto>> GetCartDetailsAsync(Guid cartId);
 
-//         // Thêm mới cart (thường dùng khi khởi tạo lần đầu)
-//         void AddCart(Cart cart);
+        // Thêm chi tiết (CartDetail)
+        Task AddDetailAsync(Guid userId, Guid bookId, int quantity);
 
-//         // Cập nhật cart (ví dụ cập nhật CreatedCart hoặc thông tin chung)
-//         void UpdateCart(Cart cart);
+        // Cập nhật chi tiết
+        Task UpdateDetailAsync(Guid userId, Guid bookId, int quantity);
 
-//         // Xóa cart (nếu có nhu cầu)
-//         void RemoveCart(Cart cart);
+        // Xóa chi tiết
+        Task RemoveDetailAsync(Guid userId, Guid bookId);
 
-//         // Thêm chi tiết (CartDetail)
-//         void AddDetail(CartDetail detail);
-
-//         // Cập nhật chi tiết
-//         void UpdateDetail(CartDetail detail);
-
-//         // Xóa chi tiết
-//         void RemoveDetail(CartDetail detail);
-
-//         // Ghi vào DB
-//         Task SaveChangesAsync();
-//     }
-// }
+        // Clear cart
+        Task ClearCartAsync(Guid userId);
+    }
+}
--- StackBook/Services/CartService.cs
+++ StackBook/Services/CartService.cs
@@ -1,125 +1,125 @@
-// using StackBook.Interfaces;
-// using StackBook.Models;
-// using StackBook.Data;
-// using Microsoft.EntityFrameworkCore;
-// using System.Linq.Expressions;
-// using StackBook.Exceptions;
-// using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
-// using StackBook.DAL;
+using StackBook.Interfaces;
+using StackBook.Models;
+using StackBook.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using StackBook.Exceptions;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using StackBook.DAL;
 
-// namespace StackBook.Services
-// {
-//     public class CartService : ICartService
-//     {
-//         private readonly ApplicationDbContext _context;
-//         public CartService(ApplicationDbContext context)
-//         {
-//             _context = context;
-//         }
-//         public async Task<Cart> GetOrCreateCartAsync(Guid userId)
-//         {
-//             var cart = await _context.Carts.Include(c => c.CartDetails)
-//                 .ThenInclude(cd => cd.Book)
-//                 .FirstOrDefaultAsync(c => c.UserId == userId);
-//             if (cart == null)
-//             {
-//                 cart = new Cart[[[[[[[[[]]]]]]]]]
-//                 {
-//                     UserId = userId,
-//                     CartDetails = new List<CartDetail>()
-//                 };
-//                 _context.Carts.Add(cart);
-//                 await _context.SaveChangesAsync();
-//             }
+namespace StackBook.Services
+{
+    public class CartService : ICartService
+    {
+        private readonly ApplicationDbContext _context;
+        public CartService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Cart> GetOrCreateCartAsync(Guid userId)
+        {
+            var cart = await _context.Carts.Include(c => c.CartDetails)
+                .ThenInclude(cd => cd.Book)
+                .FirstOrDefaultAsync(c => c.UserId == userId);
+            if (cart == null)
+            {
+                cart = new CancellationTokenRegistration
+                {
+                    UserId = userId,
+                    CartDetails = new List<CartDetail>()
+                };
+                _context.Carts.Add(cart);
+                await _context.SaveChangesAsync();
+            }
 
-//             return cart;
-//         }
+            return cart;
+        }
 
-//         public async Task<Cart> GetCartAsync(Guid userId)
-//         {
-//             var cart = await GetOrCreateCartAsync(userId);
-//             foreach (var detail in cart.CartDetails)
-//             {
-//                 if(detail.Quantity > detail.Book.Stock)
-//                 {
-//                     throw new OutOfStockException($"Book {detail.Book.BookTitle} is out of stock. Available: {detail.Book.Stock}, Requested: {detail.Quantity}");
-//                 }
-//             }
-//             return cart;
-//         }
-//         public async Task<List<CartDetail>> GetCartDetailsAsync(Guid userId)
-//         {
-//             var cart = await GetOrCreateCartAsync(userId);
-//             return cart.CartDetails.ToList();
-//         }
-//         public async Task AddToCartAsync(Guid userId, Guid bookId, int quantity)
-//         {
-//             var cart = await GetOrCreateCartAsync(userId);
-//             var book = await _context.Books.FindAsync(bookId) ?? throw new KeyNotFoundException("Book not found.");
+        public async Task<Cart> GetCartAsync(Guid userId)
+        {
+            var cart = await GetOrCreateCartAsync(userId);
+            foreach (var detail in cart.CartDetails)
+            {
+                if(detail.Quantity > detail.Book.Stock)
+                {
+                    throw new OutOfStockException($"Book {detail.Book.BookTitle} is out of stock. Available: {detail.Book.Stock}, Requested: {detail.Quantity}");
+                }
+            }
+            return cart;
+        }
+        public async Task<List<CartDetail>> GetCartDetailsAsync(Guid userId)
+        {
+            var cart = await GetOrCreateCartAsync(userId);
+            return cart.CartDetails.ToList();
+        }
+        public async Task AddToCartAsync(Guid userId, Guid bookId, int quantity)
+        {
+            var cart = await GetOrCreateCartAsync(userId);
+            var book = await _context.Books.FindAsync(bookId) ?? throw new KeyNotFoundException("Book not found.");
 
-//             var cartDetail = cart.CartDetails.FirstOrDefault(cd => cd.BookId == bookId);
-//             var newQty = (cartDetail?.Quantity ?? 0) + quantity;
-//             if (newQty > book.Stock)
-//             {
-//                 throw new OutOfStockException($"Book {book.BookTitle} is out of stock. Available: {book.Stock}, Requested: {newQty}");
-//             }
-//             if (cartDetail == null)
-//             {
-//                 cartDetail = new CartDetail
-//                 {
-//                     BookId = bookId,
-//                     Quantity = quantity,
-//                     Book = book
-//                 };
-//                 cart.CartDetails.Add(cartDetail);
-//             }
-//             else
-//             {
-//                 cartDetail.Quantity = newQty;
-//             }
-//             await _context.SaveChangesAsync();
-//         }
-//         public async Task UpdateCartAsync(Guid userId, Guid bookId, int quantity)
-//         {
-//             var cart = await GetOrCreateCartAsync(userId);
-//             var cartDetail = cart.CartDetails.FirstOrDefault(cd => cd.BookId == bookId) ?? throw new KeyNotFoundException("Book not found in cart.");
-//             var book = await _context.Books.FindAsync(bookId) ?? throw new KeyNotFoundException("Book not found.");
-//             if(quantity <= 0)
-//             {
-//                 cart.CartDetails.Remove(cartDetail);
-//             }
-//             else
-//             {
-//                 if (quantity > book.Stock)
-//                 {
-//                     throw new OutOfStockException($"Book {book.BookTitle} is out of stock. Available: {book.Stock}, Requested: {quantity}");
-//                 }
-//                 cartDetail.Quantity = quantity;
-//             }
-//             await _context.SaveChangesAsync();
-//         }
-//         public async Task RemoveFromCartAsync(Guid userId, Guid bookId)
-//         {
-//             var cart = await GetOrCreateCartAsync(userId);
-//             var cartDetail = cart.CartDetails.FirstOrDefault(cd => cd.BookId == bookId) ?? throw new KeyNotFoundException("Book not found in cart.");
-//             if(cartDetail != null)
-//             {
-//                 cart.CartDetails.Remove(cartDetail);
-//                 await _context.SaveChangesAsync();
-//             }
-//         }
-//         public async Task ClearCartAsync(Guid userId, bool clearAll = false)
-//         {
-//             var cart = await GetOrCreateCartAsync(userId);
-//             if (clearAll)
-//             {
-//                 _context.Carts.Remove(cart);
-//             }
-//             else
-//             {
-//                 cart.CartDetails.Clear();
-//             }
-//             await _context.SaveChangesAsync();
-//         }
-//     }
-// }
+            var cartDetail = cart.CartDetails.FirstOrDefault(cd => cd.BookId == bookId);
+            var newQty = (cartDetail?.Quantity ?? 0) + quantity;
+            if (newQty > book.Stock)
+            {
+                throw new OutOfStockException($"Book {book.BookTitle} is out of stock. Available: {book.Stock}, Requested: {newQty}");
+            }
+            if (cartDetail == null)
+            {
+                cartDetail = new CartDetail
+                {
+                    BookId = bookId,
+                    Quantity = quantity,
+                    Book = book
+                };
+                cart.CartDetails.Add(cartDetail);
+            }
+            else
+            {
+                cartDetail.Quantity = newQty;
+            }
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateCartAsync(Guid userId, Guid bookId, int quantity)
+        {
+            var cart = await GetOrCreateCartAsync(userId);
+            var cartDetail = cart.CartDetails.FirstOrDefault(cd => cd.BookId == bookId) ?? throw new KeyNotFoundException("Book not found in cart.");
+            var book = await _context.Books.FindAsync(bookId) ?? throw new KeyNotFoundException("Book not found.");
+            if(quantity <= 0)
+            {
+                cart.CartDetails.Remove(cartDetail);
+            }
+            else
+            {
+                if (quantity > book.Stock)
+                {
+                    throw new OutOfStockException($"Book {book.BookTitle} is out of stock. Available: {book.Stock}, Requested: {quantity}");
+                }
+                cartDetail.Quantity = quantity;
+            }
+            await _context.SaveChangesAsync();
+        }
+        public async Task RemoveFromCartAsync(Guid userId, Guid bookId)
+        {
+            var cart = await GetOrCreateCartAsync(userId);
+            var cartDetail = cart.CartDetails.FirstOrDefault(cd => cd.BookId == bookId) ?? throw new KeyNotFoundException("Book not found in cart.");
+            if(cartDetail != null)
+            {
+                cart.CartDetails.Remove(cartDetail);
+                await _context.SaveChangesAsync();
+            }
+        }
+        public async Task ClearCartAsync(Guid userId, bool clearAll = false)
+        {
+            var cart = await GetOrCreateCartAsync(userId);
+            if (clearAll)
+            {
+                _context.Carts.Remove(cart);
+            }
+            else
+            {
+                cart.CartDetails.Clear();
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
+}

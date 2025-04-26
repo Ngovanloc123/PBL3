8dd8015aa5f791e794d8cf39bff6087047c9bfa7
haduckien <haduckien1709@gmail.com>
2025-04-26 10:02:15 +0700

    feat: database User

--- StackBook/Models/User.cs
+++ StackBook/Models/User.cs
@@ -18,20 +18,31 @@ namespace StackBook.Models
         [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
         [Required(ErrorMessage = "Email is required.")]
         public string? Email { get; set; }
+        
         public string? Password { get; set; }
+
         [Required]
-        public bool Role { get; set; }
+        public bool Role { get; set; } // true = Admin, false = User
+        
         public DateTime CreatedUser { get; set; }
+
         public string? GoogleId { get; set; }
         public string? ResetPasswordToken { get; set; }
         public DateTime? ResetTokenExpiry { get; set; }
 
         public bool LockStatus { get; set; } = true;
         public DateTime? DateLock { get; set; }
+        
         public int AmountOfTime { get; set; }
+        
         public bool IsEmailVerified { get; set; } = false;
         public DateTime? EmailVerifiedAt { get; set; }
+
         [StringLength(100)]
         public string? VerificationToken { get; set; }
+        
+        // Các trường liên quan đến Refresh Token
+        public string? RefreshToken { get; set; } // Thêm RefreshToken để lưu trữ Refresh Token
+        public DateTime? RefreshTokenExpiry { get; set; } // Thêm thời gian hết hạn của Refresh Token
     }
 }
